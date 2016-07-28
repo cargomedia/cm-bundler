88d7b5f4e6003a10d07ac1f4da0c1e724cfc0ec7
Michel Meyer <michel.meyer@cargomedia.ch>
2016-07-28 16:33:43 +0200

    Use commander for the cmd

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
-var output = require('../lib/stream/output');
 
 try {
-  require('../lib/bundler')
-    .process()
-    .pipe(output());
+  var version = require('../package.json').version;
+  var program = require('commander');
+  var output = require('../lib/stream/output');
+
+  var jsonConfig = null;
+
+  program
+    .version(version)
+    .arguments('[json]')
+    .action(function (json) {
+      jsonConfig = JSON.parse(json);
+    });
+
+  program.parse(process.argv);
+
+  if (!jsonConfig || !program.args.length > 1) {
+    program.outputHelp();
+    process.exit(1);
+  } else {
+    require('../lib/bundler')
+      .process(jsonConfig)
+      .pipe(output());
+  }
 
 } catch (error) {
   process.stderr.write(error.stack + "\n");
