090cbe67320c0d5db77a30eed3c62e5f03afc3b3
Michel Meyer <michel.meyer@cargomedia.ch>
2016-07-29 11:52:41 +0200

    Add code, sourcemaps, all sub-command

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,39 +3,54 @@ try {
   var sourcemaps = require('gulp-sourcemaps');
   var program = require('commander');
   var version = require('../package.json').version;
+  var condition = require('../lib/stream/condition');
   var output = require('../lib/stream/output');
 
   var jsonConfig = null;
+  var options = {
+    code: false,
+    sourcemaps: false
+  };
+
+  program.version(version);
 
   program
-    .version(version)
-    .option('-c, --code-only', 'output the source code only')
-    .option('-s, --sourcemaps-only', 'output the sourcemaps only')
-    .option('-n, --nice', 'JSON formatting.')
-    .arguments('<json>')
+    .command('code <json>')
+    .description('generate the bundle code')
+    .action(function(json) {
+      options.code = true;
+      jsonConfig = JSON.parse(json);
+    });
+
+  program
+    .command('sourcemaps <json>')
+    .description('generate the sourcemaps')
+    .action(function(json) {
+      options.sourcemaps = true;
+      jsonConfig = JSON.parse(json);
+    });
+
+  program
+    .command('all <json>')
+    .description('generate the bundle code + inline sourcemaps')
     .action(function(json) {
       jsonConfig = JSON.parse(json);
     });
 
   program.parse(process.argv);
 
-  if (!jsonConfig || !program.args.length > 1) {
+  if (!jsonConfig || program.args.length > 2) {
     program.outputHelp();
     process.exit(1);
     return;
   }
 
-  if (program.sourcemapsOnly) {
-    jsonConfig.sourceMaps = true;
-  }
-
   require('../lib/bundler')
     .process(jsonConfig)
-    .pipe(output({
-      code: program.codeOnly,
-      sourcemaps: program.sourcemapsOnly,
-      nice: program.nice
-    }));
+    .pipe(condition(!options.code && !options.sourcemaps, function() {
+      return sourcemaps.write();
+    }))
+    .pipe(output(options));
 
 } catch (error) {
   process.stderr.write(error.stack + "\n");
