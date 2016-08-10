14e5b75ce69fcdd5919a9240ddd58a0dc0532083
Michel Meyer <michel.meyer@cargomedia.ch>
2016-08-10 11:59:17 +0200

    Update bundler entries tests

--- test/bundler/entries.js
+++ test/bundler/entries.js
@@ -1,30 +1,32 @@
 var vm = require('vm');
+var path = require('path');
 var assert = require('chai').assert;
+var deap = require('deap');
 var browserify = require('browserify');
 var convert = require('convert-source-map');
-var bundler = require('../../lib/bundler');
-var path = require('path');
 
 var dataDir = path.join(__dirname, '..', '_data');
 var baseDir = path.join(dataDir, 'base');
 var libDir = path.join(dataDir, 'lib');
 
+var entries = require('../../lib/bundler/entries');
+
 var executeInVM = function(src) {
   var context = {
     console: console,
-    execCount: {},
+    callCount: {},
     loadCount: {},
     moduleLoaded: function(name) {
-      context.loadCount[name] = name in context.loadCount ? context.loadCount[name]++ : 1;
+      context.loadCount[name] = context.getCountLoaded(name) + 1;
     },
     getCountLoaded: function(name) {
       return name in context.loadCount ? context.loadCount[name] : 0;
     },
-    moduleExecuted: function(name) {
-      context.execCount[name] = name in context.execCount ? context.execCount[name]++ : 1;
+    moduleCalled: function(name) {
+      context.callCount[name] = context.getCountCalled(name) + 1;
     },
-    getCountExecuted: function(name) {
-      return name in context.execCount ? context.execCount[name] : 0;
+    getCountCalled: function(name) {
+      return name in context.callCount ? context.callCount[name] : 0;
     }
   };
   vm.runInNewContext(src, context);
@@ -35,15 +37,16 @@ var executeInVM = function(src) {
 describe('bundler: add entries', function() {
 
   it('module not found', function(done) {
-    var b = browserify({
-      basedir: baseDir
-    });
+    var options = deap(
+      {debug: true},
+      entries.prepare([
+        'wrong/file1.js'
+      ])
+    );
 
-    bundler.addEntries(b, [
-      'wrong/file1.js'
-    ]);
-
-    b.bundle(function(error, src) {
+    entries
+      .process(browserify(options))
+      .bundle(function(error, src) {
       assert.instanceOf(error, Error);
       assert.match(error.toString(), /Cannot find module/);
       done();
@@ -51,73 +54,82 @@ describe('bundler: add entries', function() {
   });
 
   it('no dependencies', function(done) {
-    var b = browserify({
-      basedir: baseDir
-    });
+    var options = deap(
+      {
+        basedir: baseDir
+      },
+      entries.prepare([
+        'foo/file1.js'
+      ])
+    );
 
-    bundler.addEntries(b, [
-      'foo/file1.js'
-    ]);
-
-    b.bundle(function(error, src) {
-      assert.ifError(error);
+    entries
+      .process(browserify(options))
+      .bundle(function(error, src) {
+        assert.ifError(error);
       assert.isObject(src);
       assert.ok(src.length > 0);
 
       var context = executeInVM(src);
       assert.isUndefined(context.require);
       assert.equal(context.getCountLoaded('foo/file1'), 1);
-      assert.equal(context.getCountExecuted('foo/file1'), 0);
+        assert.equal(context.getCountCalled('foo/file1'), 0);
       done();
     });
   });
 
   it('with local dependencies', function(done) {
-    var b = browserify({
-      basedir: baseDir
-    });
+    var options = deap(
+      {
+        basedir: baseDir
+      },
+      entries.prepare([
+        'foo/file2.js'
+      ])
+    );
 
-    bundler.addEntries(b, [
-      'foo/file2.js'
-    ]);
-
-    b.bundle(function(error, src) {
-      assert.ifError(error);
+    entries
+      .process(browserify(options))
+      .bundle(function(error, src) {
+        assert.ifError(error);
       assert.isObject(src);
       assert.ok(src.length > 0);
 
       var context = executeInVM(src);
       assert.isUndefined(context.require);
       assert.equal(context.getCountLoaded('foo/file2'), 1);
-      assert.equal(context.getCountExecuted('foo/file2'), 0);
+        assert.equal(context.getCountCalled('foo/file2'), 0);
       assert.equal(context.getCountLoaded('foo/file1'), 1);
-      assert.equal(context.getCountExecuted('foo/file1'), 0);
+        assert.equal(context.getCountCalled('foo/file1'), 0);
       done();
     });
   });
 
 
   it('with external dependencies', function(done) {
-    var b = browserify({
-      basedir: baseDir,
-      paths: [libDir]
-    });
+    var options = deap(
+      {
+        basedir: baseDir,
+        paths: [libDir]
+      },
+      entries.prepare([
+        'foo/file3.js'
+      ])
+    );
 
-    bundler.addEntries(b, [
-      'foo/file3.js'
-    ]);
-
-    b.bundle(function(error, src) {
-      assert.ifError(error);
+    entries
+      .process(browserify(options))
+      .bundle(function(error, src) {
+        assert.ifError(error);
       assert.isObject(src);
       assert.ok(src.length > 0);
 
       var context = executeInVM(src);
       assert.isUndefined(context.require);
       assert.equal(context.getCountLoaded('foo/file3'), 1);
-      assert.equal(context.getCountExecuted('foo/file3'), 0);
+        assert.equal(context.getCountCalled('foo/file3'), 0);
       assert.equal(context.getCountLoaded('bar/file1'), 1);
-      assert.equal(context.getCountExecuted('bar/file1'), 0);
+        assert.equal(context.getCountCalled('bar/file1'), 0);
       done();
     });
   });
